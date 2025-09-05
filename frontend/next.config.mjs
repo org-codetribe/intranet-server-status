92cb710848a647f025e30f7d3d44136ec6dc8504
CTS <appdev.cts@gmail.com>
2025-09-05 10:54:26 +0530

    added config for vercel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: false, // makes sure it uses Pages Router, not App Router
+  },
 };
 
 export default nextConfig;
